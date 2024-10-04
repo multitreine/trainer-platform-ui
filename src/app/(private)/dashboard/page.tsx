1762907f40b621b6c7755c837578821caec3ba2f
samuel vermeuln <vermeuln@ucl.br>
2024-10-03 23:13:42 -0300

    .

---
+++ src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/HeroSection";
+import { CoursesSection } from "@/components/CoursesSection";
+import { TeachersSection } from "@/components/TeachersSection";
+import { StructureSection } from "@/components/StructureSection";
+import { NewsletterSection } from "@/components/NewsletterSection";
+import { Footer } from "@/components/Footer";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-white">
+      <h1>PAGINA PRIVADA</h1>
+    </div>
+  );
+}
