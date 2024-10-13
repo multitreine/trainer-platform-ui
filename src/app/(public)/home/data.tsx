e9549d0f5045d621dc73671b1b4b2898f9e20656
samuel vermeuln <vermeuln@ucl.br>
2024-10-13 16:37:08 -0300

    ..

--- src/app/(public)/home/data.tsx
+++ src/app/(public)/home/data.tsx
@@ -1,4 +1,4 @@
-"use server";
+
 import { makeStore } from "@/store/createStore";
 import { operationsHeader } from "@/ducks/header"; 
 import { operationsHero } from "@/ducks/hero";
@@ -9,7 +9,7 @@ import { operationsFooter } from "@/ducks/footer";
 
 const wrapperDataPage = (Component: any) => {
   return async function WrapperDataPage() {
-    const dispatch = makeStore.dispatch;
+    const dispatch = makeStore?.dispatch || (() => {});
 
     await Promise.all([
       dispatch(operationsHeader.fetchHeaderIfNeeded()),
