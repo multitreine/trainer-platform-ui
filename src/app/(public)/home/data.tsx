fcbffe8c3c62f0b7f85933e178b1de77b850f66a
samuel vermeuln <vermeuln@ucl.br>
2024-10-13 16:40:07 -0300

    try cath

--- src/app/(public)/home/data.tsx
+++ src/app/(public)/home/data.tsx
@@ -11,14 +11,19 @@ const wrapperDataPage = (Component: any) => {
   return async function WrapperDataPage() {
     const dispatch = makeStore?.dispatch || (() => {});
 
-    await Promise.all([
-      dispatch(operationsHeader.fetchHeaderIfNeeded()),
-      dispatch(operationsHero.fetchHeroIfNeeded()),
-      dispatch(operationsCourses.fetchCoursesIfNeeded()),
-      dispatch(operationsTeachers.fetchTeachersData()),
-      dispatch(operationsStructure.fetchStructureData()),
-      dispatch(operationsFooter.fetchFooterIfNeeded()),
-    ]);
+    try {
+      await Promise.all([
+        dispatch(operationsHeader.fetchHeaderIfNeeded()),
+        dispatch(operationsHero.fetchHeroIfNeeded()),
+        dispatch(operationsCourses.fetchCoursesIfNeeded()),
+        dispatch(operationsTeachers.fetchTeachersData()),
+        dispatch(operationsStructure.fetchStructureData()),
+        dispatch(operationsFooter.fetchFooterIfNeeded()),
+      ]);
+    } catch (error) {
+      console.error("error error wrapperDataPage all dispatch", JSON.stringify(error, null, 2));
+      Promise.resolve();
+    }
 
     return <Component />;
   };
